d11ccafa5f5c5d562bb674032ece7f0f9b7ba0ca
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-02-16 12:23:04 -0300

    aula

---
+++ src/pages/api/users/[...params].tsx
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const users = (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query);
+
+  const users = [
+    { id: 1, name: "Marco" },
+    { id: 2, name: "Thiago" },
+    { id: 3, name: "Ana" },
+  ];
+
+  return response.json(users);
+};
+
+export default users;
