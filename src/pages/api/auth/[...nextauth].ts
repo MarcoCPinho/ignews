ccc240eba2064b7d4317534956010fd7eb79cd72
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-02-28 12:44:23 -0300

    aula

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,16 @@
-import NextAuth from "next-auth";
+import { query as q } from "faunadb";
+import NextAuth, { Account, Profile, User } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
+
+interface signInProps {
+  user: User;
+  account: Account;
+  profile: Profile;
+  email: {
+    verificationRequest?: boolean;
+  };
+}
 
 export default NextAuth({
   providers: [
@@ -8,4 +19,16 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }: signInProps) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection("users"), { data: { email } }));
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,5 +1,6 @@
 import { Client } from "faunadb";
 
-export const faunta = new Client({
+export const fauna = new Client({
   secret: process.env.FAUNADB_KEY,
+  domain: "db.us.fauna.com",
 });
