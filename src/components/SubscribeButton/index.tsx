0409d7f8509469375a9288074615172eb713e177
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-03-08 13:24:16 -0300

    1 aula

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { useSession, signIn } from "next-auth/react";
+import { useCallback } from "react";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -5,8 +7,23 @@ interface SubscribeButtonProps {
 }
 
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { data: session } = useSession();
+
+  const handleSubscribe = useCallback(() => {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    // criação da "checkout session"
+  }, [session]);
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
