f0a72c23341d26125034be99a19f90fd0a003779
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-03-10 18:10:32 -0300

    aula

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,7 @@
 import { useSession, signIn } from "next-auth/react";
 import { useCallback } from "react";
+import { api } from "../../services/api";
+import { getStripeJS } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -9,13 +11,23 @@ interface SubscribeButtonProps {
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   const { data: session } = useSession();
 
-  const handleSubscribe = useCallback(() => {
+  const handleSubscribe = useCallback(async () => {
     if (!session) {
       signIn("github");
       return;
     }
 
-    // criação da "checkout session"
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
   }, [session]);
 
   return (
