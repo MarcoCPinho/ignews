c81785c1e581d3d8f00757bd78c732feb6b75eab
Marco Carvalho de Pinho <marcocpinho@hotmail.com>
2022-04-27 13:02:35 -0300

    aula

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,10 +4,20 @@ import React from "react";
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
-// import { Container } from './styles';
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
 
-export const posts: React.FC = () => {
+interface PostsProps {
+  posts: Post[];
+}
+
+export const Posts = ({ posts }: PostsProps) => {
   return (
     <>
       <Head>
@@ -16,21 +26,13 @@ export const posts: React.FC = () => {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a more blabla</strong>
-            <p>conteúdo do post</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a more blabla</strong>
-            <p>conteúdo do post</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Creating a more blabla</strong>
-            <p>conteúdo do post</p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -40,7 +42,7 @@ export const posts: React.FC = () => {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
 
-  const response = await prismic.query(
+  const response = await prismic.query<any>(
     [Prismic.predicates.at("document.type", "post")],
     {
       fetch: ["post.title", "post.content"],
@@ -48,9 +50,24 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   );
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map((post) => ({
+    slug: post.uid,
+    title: RichText.asText(post.data.title),
+    excerpt: RichText.asHtml(
+      post.data.content.find((content) => content.type === "paragraph")?.text ??
+        ""
+    ),
+    updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+      "pt-BR",
+      {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }
+    ),
+  }));
 
   return {
-    props: {},
+    props: { posts },
   };
 };
